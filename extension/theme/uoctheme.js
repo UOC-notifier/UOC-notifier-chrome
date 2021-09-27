3d73f3e455f53a276a5ff7cccf5bfa98a669ac10
John <jmillantr@gmail.com>
2021-09-27 12:58:09 +0200

    Customize UOC theme to make it looks like UOC Notifier

--- extension/theme/uoctheme.js
+++ extension/theme/uoctheme.js
@@ -34,17 +34,47 @@ function containerFluid(){
         }
 }
 
+function betterModulWidgets() {
+        var moduls = document.getElementsByClassName('modul');
+        for(var i = 0; i < moduls.length; i++) {
+                // Ensure show content to enable toggle click
+                var toggleMenuDisabled = moduls[i].querySelector('.toggleMenu .icon--arrow-down');
+                if (toggleMenuDisabled) {
+                        toggleMenuDisabled.parentElement.click();
+                }
+
+                var modulHeader = moduls[i].getElementsByClassName('modul__header')[0];
+                var modulContent = moduls[i].getElementsByClassName('modul__content')[0];
+                var modulMarcadors = moduls[i].getElementsByClassName('widget__marcadors')[0];
+                var modulNotification = modulContent.querySelector('.widget__marcadors .marcadors');
+
+                // Move notifications
+                if (modulNotification) {
+                        modulHeader.getElementsByClassName('modul__actions')[0].prepend(modulNotification);
+                        modulMarcadors.remove();
+                }
+
+                // Hide/show container
+                if (modulHeader.classList.contains('toggle_content_added')) continue;
+                modulHeader.classList.add('toggle_content_added');
+                modulHeader.addEventListener('click', function(e) {
+                        this.parentElement.classList.toggle('show_content');
+                });
+        }
+}
+
 chrome.runtime.sendMessage({uoctheme: "hello"}, function(response) {
-  if( response.active ){
+  if( response.active ) {
     var interval = setInterval(myTimer, 1000);
     var countInterval=0;
 
     function myTimer() {
-        if( ++countInterval > 5 ){
-	        stopTimer()
+        if(++countInterval > 5) {
+                stopTimer()
         }
-        removeNoticies();
-        containerFluid();
+        // removeNoticies();
+        // containerFluid();
+        betterModulWidgets();
     }
 
     function stopTimer() {
